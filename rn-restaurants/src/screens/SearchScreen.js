a37357ae80504e35b1fc03dcefdc7cc38ce07ab9
Manoj Silag <silagmanoj@yahoo.in>
2021-05-03 19:50:06 +0530

    Adding yelp apis

--- rn-restaurants/src/screens/SearchScreen.js
+++ rn-restaurants/src/screens/SearchScreen.js
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (search) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: search,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pizza");
+  }, []);
 
   return (
     <View>
-      <SearchBar 
-      value={search} 
-      SetValue={setSearch} 
-      onTermSubmit={() => console.log('sub mit')}/>
-      <Text>{search}</Text>
+      <SearchBar
+        value={search}
+        SetValue={setSearch}
+        onTermSubmit={() => searchApi(search)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text>: null}
+      <Text>{results.length}</Text>
     </View>
   );
 };
